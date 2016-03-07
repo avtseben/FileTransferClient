590a1cdc1aa697873879bb62ca4943bb2278149c
Alexander Tsebenko <sas@mbh.mbhtech>
2016-03-07 21:54:14 +0600

    add_String_Transfer_mode

--- src/ClientSide.java
+++ src/ClientSide.java
@@ -6,7 +6,7 @@ public class ClientSide {
     private Socket s;
     private String host;
     private int port;
-    private FileReader fr;
+    private boolean byteTransferMode = false;
 
     public ClientSide(String _host, int _port) {
 	host = _host;
@@ -15,19 +15,24 @@ public class ClientSide {
     }
     public void run() {
 	try {
-	 //   fr = new FileReader("infile");//TODO Transfer name of file
 	    BufferedReader in = new BufferedReader(new FileReader("infile"));//TODO Transfer name of file
 	    try {
 		s.connect(new InetSocketAddress(host,port));
 		OutputStream outStream = s.getOutputStream();
+		PrintWriter out = new PrintWriter(outStream, true);
 		String line = new String();
 		      while((line = in.readLine()) != null) {
+			if(byteTransferMode){
 			  byte [] lineArrayByte = line.getBytes("utf-8"); 
 			  for(int i = 0; i < lineArrayByte.length; i++)  
 			      outStream.write(lineArrayByte[i]);
 			  outStream.write('\n');
+			} else {
+			    out.println(line);
+			}
 		    }
 		s.close();
+		out.close();
 
 	    } catch (Exception e) {
 		System.err.println("Cannot connect");
